d7d2b8dccda13bc4a7ecff6550018c99412de74d
danny-molnar <danny.molnar@appvia.io>
2025-01-14 13:21:36 +0000

    chore: del data src as not needed

--- modules/budgets/data.tf
+++
@@ -1,6 +0,0 @@
-# Retrieve SNS topic for budget notifications
-# data "aws_sns_topic" "budget_notifications" {
-#   name = var.sns_topic_name
-
-#   depends_on = [module.notifications]
-# }
--- modules/budgets/main.tf
+++ modules/budgets/main.tf
@@ -68,10 +68,9 @@ resource "aws_budgets_budget" "this" {
     comparison_operator        = each.value.notification.comparison_operator
     notification_type          = each.value.notification.notification_type
     subscriber_email_addresses = var.notifications.email != null ? var.notifications.email.addresses : null
-    # subscriber_sns_topic_arns  = [data.aws_sns_topic.budget_notifications.arn]
-    subscriber_sns_topic_arns = [module.notifications.sns_topic_arn]
-    threshold                 = each.value.notification.threshold
-    threshold_type            = each.value.notification.threshold_type
+    subscriber_sns_topic_arns  = [module.notifications.sns_topic_arn]
+    threshold                  = each.value.notification.threshold
+    threshold_type             = each.value.notification.threshold_type
   }
 
   lifecycle {
