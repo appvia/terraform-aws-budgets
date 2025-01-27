092e0aa45cff3c17b00b7a2d3eee7a1af4a4fa40
Rohith Jayawardene <gambol99@gmail.com>
2025-01-27 18:26:47 +0000

    chore(deps): bumping to the latest version of the module

--- modules/budgets/main.tf
+++ modules/budgets/main.tf
@@ -2,7 +2,7 @@
 ## Provision the SNS topic for the budgets if required and notifications
 module "notifications" {
   source  = "appvia/notifications/aws"
-  version = "2.0.0"
+  version = "2.0.1"
 
   allowed_aws_services = [
     "budgets.amazonaws.com",
